2500a27647f8fe08ccb1ef17529134de9593007e
Raghav Anand <raghav_anand98@yahoo.com>
2020-04-22 23:46:20 -0700

    scripting changes

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -4,6 +4,7 @@ parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--num_trials', type=int, required=True)
 parser.add_argument('--num_samples', default="1")
 parser.add_argument('--time_limits', default="1,5,10")
+parser.add_argument('--algorithm', action='append', required=True)
 parser.add_argument('--scenario', action='append', required=True)
 parser.add_argument('--local', type=bool, default=True)
 parser.add_argument('--load_graph', dest='load_graph', action='store_true', default=False)
@@ -13,7 +14,7 @@ args.time_limits = [float(time_limit) for time_limit in args.time_limits.split("
 args.num_samples = [int(num_sample) for num_sample in args.num_samples.split(",")]
 
 
-def run_fetch_env_frame1(args, time_limit, num_sample, folder_name, num_division):
+def run_fetch_env_frame1(args, time_limit, num_sample, folder_name, num_division, algorithm):
     # env-frame: 0.48...
     starts = [[0.100000001490116, 1.57079637050629, 1.57079637050629, 0, 1.57079637050629, 0, 1.57079637050629, 0]]
     goals = [
@@ -68,8 +69,10 @@ def run_fetch_env_frame1(args, time_limit, num_sample, folder_name, num_division
         os.makedirs(folder_name)
         load_graph = '""' # empty string
         
-
-    command += " --lambda_type {lambda_type} --coordinator {coordinator} --num_samples {num_samples} --num_divisions {num_divisions} --time-limit {time_limit} --load_graph {load_graph}".format(num_samples=num_sample, num_divisions=num_division, time_limit=time_limit, lambda_type=lambda_type, coordinator=coordinator, load_graph=load_graph)
+    if algorithm == "prm_fixed_graph":
+        command += " --lambda_type {lambda_type} --coordinator {coordinator} --num_samples {num_samples} --num_divisions {num_divisions} --time-limit {time_limit} --algorithm {algorithm} --load_graph {load_graph}".format(num_samples=num_sample, num_divisions=num_division, time_limit=time_limit, lambda_type=lambda_type, coordinator=coordinator, load_graph=load_graph, algorithm=algorithm, )
+    else:
+        command += " --lambda_type {lambda_type} --coordinator {coordinator} --num_samples {num_samples} --jobs {num_divisions} --time-limit {time_limit} --algorithm {algorithm} --load_graph {load_graph}".format(num_samples=num_sample, num_divisions=num_division, time_limit=time_limit, lambda_type=lambda_type, coordinator=coordinator, load_graph=load_graph, algorithm=algorithm,)
     print(command)
     out_path = "{}/out.txt".format(folder_name)
     if args.load_graph:
@@ -87,7 +90,7 @@ def run_fetch_env_frame1(args, time_limit, num_sample, folder_name, num_division
         subprocess.call("mv graph-out.txt {folder_name}".format(folder_name=folder_name), shell=True)
 
 
-def run_fetch_env_frame2(args, time_limit, num_sample, folder_name, num_division):
+def run_fetch_env_frame2(args, time_limit, num_sample, folder_name, num_division, algorithm):
     # env-frame: 0.57...
     starts = [[0.100000001490116, 1.57079637050629, 1.57079637050629, 0, 1.57079637050629, 0, 1.57079637050629, 0]]
     goals = [
@@ -142,7 +145,10 @@ def run_fetch_env_frame2(args, time_limit, num_sample, folder_name, num_division
         load_graph = '""' # empty string
         
 
-    command += " --lambda_type {lambda_type} --coordinator {coordinator} --num_samples {num_samples} --num_divisions {num_divisions} --time-limit {time_limit} --load_graph {load_graph}".format(num_samples=num_sample, num_divisions=num_division, time_limit=time_limit, lambda_type=lambda_type, coordinator=coordinator, load_graph=load_graph)
+    if algorithm == "prm_fixed_graph":
+        command += " --lambda_type {lambda_type} --coordinator {coordinator} --num_samples {num_samples} --num_divisions {num_divisions} --time-limit {time_limit} --algorithm {algorithm} --load_graph {load_graph}".format(num_samples=num_sample, num_divisions=num_division, time_limit=time_limit, lambda_type=lambda_type, coordinator=coordinator, load_graph=load_graph, algorithm=algorithm, )
+    else:
+        command += " --lambda_type {lambda_type} --coordinator {coordinator} --num_samples {num_samples} --jobs {num_divisions} --time-limit {time_limit} --algorithm {algorithm} --load_graph {load_graph}".format(num_samples=num_sample, num_divisions=num_division, time_limit=time_limit, lambda_type=lambda_type, coordinator=coordinator, load_graph=load_graph, algorithm=algorithm,)
     print(command)
     out_path = "{}/out.txt".format(folder_name)
     if args.load_graph:
@@ -159,7 +165,7 @@ def run_fetch_env_frame2(args, time_limit, num_sample, folder_name, num_division
             subprocess.call("mv lambda-* {folder_name}".format(folder_name=folder_name), shell=True)
         subprocess.call("mv graph-out.txt {folder_name}".format(folder_name=folder_name), shell=True)
 
-def run_png(args, time_limit, num_sample, folder_name, num_division):
+def run_png(args, time_limit, num_sample, folder_name, num_division, algorithm):
 
     command = "make -j8; ./mpl_coordinator --scenario png --global_min 0,0 --global_max 1403,1404 --env resources/house_layout.png --start 1301,332 --goal 600,1034 --goal 275,87 --goal 789,709 --goal 533,822 --goal 357,142 --goal 884,1173 --goal 446,898 --goal 82,478 --goal 952,339 --goal 6,394"
     if args.local:
@@ -181,7 +187,10 @@ def run_png(args, time_limit, num_sample, folder_name, num_division):
         load_graph = '""' # empty string
         
 
-    command += " --lambda_type {lambda_type} --coordinator {coordinator} --num_samples {num_samples} --num_divisions {num_divisions} --time-limit {time_limit} --load_graph {load_graph}".format(num_samples=num_sample, num_divisions=num_division, time_limit=time_limit, lambda_type=lambda_type, coordinator=coordinator, load_graph=load_graph)
+    if algorithm == "prm_fixed_graph":
+        command += " --lambda_type {lambda_type} --coordinator {coordinator} --num_samples {num_samples} --num_divisions {num_divisions} --time-limit {time_limit} --algorithm {algorithm} --load_graph {load_graph}".format(num_samples=num_sample, num_divisions=num_division, time_limit=time_limit, lambda_type=lambda_type, coordinator=coordinator, load_graph=load_graph, algorithm=algorithm, )
+    else:
+        command += " --lambda_type {lambda_type} --coordinator {coordinator} --num_samples {num_samples} --jobs {num_divisions} --time-limit {time_limit} --algorithm {algorithm} --load_graph {load_graph}".format(num_samples=num_sample, num_divisions=num_division, time_limit=time_limit, lambda_type=lambda_type, coordinator=coordinator, load_graph=load_graph, algorithm=algorithm,)
     print(command)
     out_path = "{}/out.txt".format(folder_name)
     if args.load_graph:
@@ -204,49 +213,53 @@ if __name__ == "__main__":
     #time_limits = [1, 5, 10, 20, 30] #[0.2, 0.5, ]#1]
     #num_samples = [1, ] #10, 20, 40]
     #num_trials = 30
-    for scenario in args.scenario:
-        if scenario == "png":
-            num_divisions = [
-                (0,0),
-                (1,1),
-                (2,2),
-            ]
-        else:
-            num_divisions = [
-                (0,0,0,0,0,0,0,0),  #1
-                #(1,0,0,0,0,0,0,0),  #2
-                (1,1,0,0,0,0,0,0),  #4
-                #(2,1,0,0,0,0,0,0),  #6
-                (1,1,1,0,0,0,0,0),  #8
-                #(1,1,1,1,0,0,0,0),  #16
-                #(1,1,1,1,1,0,0,0),  #32
-                #(1,1,1,1,1,1,0,0),  #64
-                #(1,1,1,1,1,1,1,0),  #128
+    for algorithm in args.algorithm:
+        for scenario in args.scenario:
+            if scenario == "png":
+                num_divisions = [
+                    (0,0),
+                    (1,1),
+                    (2,2),
+                ]
+            else:
+                num_divisions = [
+                    (0,0,0,0,0,0,0,0),  #1
+                    #(1,0,0,0,0,0,0,0),  #2
+                    (1,1,0,0,0,0,0,0),  #4
+                    #(2,1,0,0,0,0,0,0),  #6
+                    (1,1,1,0,0,0,0,0),  #8
+                    #(1,1,1,1,0,0,0,0),  #16
+                    #(1,1,1,1,1,0,0,0),  #32
+                    #(1,1,1,1,1,1,0,0),  #64
+                    #(1,1,1,1,1,1,1,0),  #128
 
-            ]
-        root = "outputs/{}".format(scenario)
-        if not os.path.exists(root):
-            os.makedirs(root)
-        for time_limit in args.time_limits:
-            for num_sample in args.num_samples:
-                for num_division_tup in num_divisions:
+                ]
+            root = "outputs/{}/{}".format(algorithm, scenario)
+            if not os.path.exists(root):
+                os.makedirs(root)
+            for time_limit in args.time_limits:
+                for num_sample in args.num_samples:
                     for trial in range(args.num_trials):
-                        
-                        num_lambdas = reduce(lambda a,b: (a) * (b), [val + 1 for val in num_division_tup])
-                        num_division = reduce(lambda a,b: "{},{}".format(a,b), num_division_tup)
-                        file_name = "{root}/num_divisions={num_divisions}__num_lambdas={num_lambdas}__time_limit={time_limit}__trial={trial}__num_samples={num_samples}".format(num_divisions=num_division, trial=trial, time_limit=time_limit, num_lambdas=num_lambdas, num_samples=num_sample, root=root)
-                        #if os.path.exists(file_name): continue
-                        if scenario == "fetch1":
-                            run_fetch_env_frame1(args, time_limit, num_sample, file_name, num_division)
-                        if scenario == "fetch2":
-                            run_fetch_env_frame2(args, time_limit, num_sample, file_name, num_division)
-                        elif scenario == "png":
-                            run_png(args, time_limit, num_sample, file_name, num_division)
+                        for num_division_tup in num_divisions:
+                            num_lambda = reduce(lambda a,b: (a) * (b), [val + 1 for val in num_division_tup])
+                            num_division = reduce(lambda a,b: "{},{}".format(a,b), num_division_tup)
+                            file_name = "{root}/num_divisions={num_divisions}__num_lambda={num_lambda}__time_limit={time_limit}__trial={trial}__num_samples={num_samples}".format(num_divisions=num_division, trial=trial, time_limit=time_limit, num_lambda=num_lambda, num_samples=num_sample, root=root)
+                            #if os.path.exists(file_name): continue
+                            if algorithm == "prm_fixed_graph":
+                                num_division_val = num_division
+                            else:
+                                num_division_val = num_lambda
+                            if scenario == "fetch1":
+                                run_fetch_env_frame1(args, time_limit, num_sample, file_name, num_division_val, algorithm)
+                            if scenario == "fetch2":
+                                run_fetch_env_frame2(args, time_limit, num_sample, file_name, num_division_val, algorithm)
+                            elif scenario == "png":
+                                run_png(args, time_limit, num_sample, file_name, num_division_val, algorithm)
 
-                        print("-------------------------------------------------------------------------------------")
-                        time.sleep(2) 
-                        #command = "rm -f png_2d_demo_output*; rm -f lambda-*; make -j8; ./mpl_coordinator --scenario png --global_min 0,0 --global_max 1403,1404 --env resources/house_layout.png --start 1301,332 --goal 600,1034 --goal 275,87 --goal 789,709 --goal 533,822 --goal 357,142 --goal 884,1173 --goal 446,898 --goal 82,478 --goal 952,339 --goal 6,394 --lambda_type local --coordinator localhost --num_samples {num_samples} --num_divisions {num_divisions} --time-limit {time_limit}".format(num_samples=num_sample, num_divisions=num_division, time_limit=time_limit, )
-                        #subprocess.call("mkdir {folder_name}; mv lambda-* {folder_name}".format(folder_name=file_name[:-4]), shell=True)
-                        #subprocess.call("mkdir {folder_name}; mv png* {folder_name}; mv lambda-* {folder_name}".format(folder_name=file_name[:-4]), shell=True)
+                            print("-------------------------------------------------------------------------------------")
+                            time.sleep(2) 
+                            #command = "rm -f png_2d_demo_output*; rm -f lambda-*; make -j8; ./mpl_coordinator --scenario png --global_min 0,0 --global_max 1403,1404 --env resources/house_layout.png --start 1301,332 --goal 600,1034 --goal 275,87 --goal 789,709 --goal 533,822 --goal 357,142 --goal 884,1173 --goal 446,898 --goal 82,478 --goal 952,339 --goal 6,394 --lambda_type local --coordinator localhost --num_samples {num_samples} --num_divisions {num_divisions} --time-limit {time_limit}".format(num_samples=num_sample, num_divisions=num_division, time_limit=time_limit, )
+                            #subprocess.call("mkdir {folder_name}; mv lambda-* {folder_name}".format(folder_name=file_name[:-4]), shell=True)
+                            #subprocess.call("mkdir {folder_name}; mv png* {folder_name}; mv lambda-* {folder_name}".format(folder_name=file_name[:-4]), shell=True)
 
 #### fetch stuff
